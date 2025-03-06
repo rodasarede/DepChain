30ba58621ed7fc0407b2b886d94eeb3a7da2b468
Rodrigo Arede <rodrigo.arede@tecnico.ulisboa.pt>
2025-03-06 18:07:20 +0000

    Added KeyDistribution class

---
+++ src/main/java/com/sec/app/util/KeyDistribution.java
@@ -0,0 +1,17 @@
+package com.sec.app.util;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyDistribution {
+    private static Map<String, PublicKey> _public_keys = new HashMap<>();
+
+    public static void addPublicKey(String id, PublicKey publicKey) {
+        _public_keys.put(id, publicKey);
+    }
+
+    public static PublicKey getPublicKey(String id) {
+        return _public_keys.get(id);
+    }
+}

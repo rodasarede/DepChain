10ecd53ab935dc30b6c08cca1ac95748b9c2ed55
Manuel <navalho.manuel@gmail.com>
2025-03-10 18:36:10 +0000

    fix compile

--- server/src/main/java/com/sec/depchain/server/BlockchainMember.java
+++ server/src/main/java/com/sec/depchain/server/BlockchainMember.java
@@ -14,107 +14,107 @@ public class BlockchainMember {
     private static PerfectLinks perfectLinks;
     private static int seqNumber = 1;
 
-    private EpochSate state;
-
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.out.println("Usage: <id>");
-            return;
+    private static EpochSate state;
+    
+        public static void main(String[] args) throws Exception {
+            if (args.length != 1) {
+                System.out.println("Usage: <id>");
+                return;
+            }
+            Id = Integer.parseInt(args[0]);
+    
+            systemMembership = new SystemMembership(
+                    "../common/src/main/java/com/sec/depchain/resources/system_membership.properties");
+    
+            if (Id == systemMembership.getLeaderId()) {
+                System.out.println("I am the leader with id: " + Id);
+                isLeader = true;
+            }
+    
+            // PORT = systemMembership.getMembershipList().get(Id).getPort();
+            // PORT = getPort(Id);
+            perfectLinks = new PerfectLinks(Id);
+            perfectLinks.setDeliverCallback(BlockchainMember::onPerfectLinksDeliver);
+    
+            // System.out.println("Blockchain Member listening on port " + PORT + "...");
         }
-        Id = Integer.parseInt(args[0]);
-
-        systemMembership = new SystemMembership(
-                "../common/src/main/java/com/sec/depchain/resources/system_membership.properties");
-
-        if (Id == systemMembership.getLeaderId()) {
-            System.out.println("I am the leader with id: " + Id);
-            isLeader = true;
-        }
-
-        // PORT = systemMembership.getMembershipList().get(Id).getPort();
-        // PORT = getPort(Id);
-        perfectLinks = new PerfectLinks(Id);
-        perfectLinks.setDeliverCallback(BlockchainMember::onPerfectLinksDeliver);
-
-        // System.out.println("Blockchain Member listening on port " + PORT + "...");
-    }
-
-    private static void onPerfectLinksDeliver(int senderId, String message) {
-        System.out.println("Received request: " + message + " from Id: " + senderId);
-        String[] messageElements = PerfectLinks.getMessageElements(message);
-        if (messageElements[0].equals("append")) {
-            String transaction = messageElements[2];
-
-            // Run consensus
-            boolean success = runConsensus(transaction);
-            String responseMessage = success ? "Transaction confirmed and appended." : "Transaction failed.";
-            String formattedMessage = "<append:" + messageElements[1] + ":" + responseMessage + ">";
-            // System.out.println(formattedMessage);
-
-            // Send confirmation response back to client
-            int destId = Integer.parseInt(message.split("\\|")[0]);
-            perfectLinks.send(destId, formattedMessage, seqNumber);
-            seqNumber++;
-        }
-    }
-    //why static
-    private boolean runConsensus(String transaction) {
-        System.out.println("Running consensus: INIT -> PROPOSE -> DECIDE");
-
-        if (!initConsensus())
-            return false;
-        if (!proposeConsensus(transaction))
-            return false;
-        return decideConsensus(transaction);
-    }
-    //why static
-    private boolean initConsensus() {
-        //initialize 
-        setState(new EpochSate(systemMembership.getNumberOfNodes()));
-        System.out.println("INIT phase successful.");
-        return true;
-    }
-    //why static?
-    private boolean proposeConsensus(String transaction) {
-        //Only the leader
-
-        if(isLeader())
-        {
-            if(this.getState().getVal() == null) // val == null
-            {
-                this.getState().setVal(transaction); // val:=v;
-                for(int nodeId: systemMembership.getMembershipList().keySet()) //for all q∈Π do 
-                {
-                    // For each process q , it triggers a Send event to send a [READ] msg doing AuthPerfectLinks
-                    //TODO
-                    // Processes reply with STATE message containing its local state <valts, val, writeset>:
-                    // Leader sends READ to all processes
-                    //how to create message???
-                    //String message = "<READ|" + getEpochId() + "|" + Id + ">";
-                    //perfectLinks.send(nodeId, message, seq_number);
-                    //seq_number ++;
-                }
-        
+    
+        private static void onPerfectLinksDeliver(int senderId, String message) {
+            System.out.println("Received request: " + message + " from Id: " + senderId);
+            String[] messageElements = PerfectLinks.getMessageElements(message);
+            if (messageElements[0].equals("append")) {
+                String transaction = messageElements[2];
+    
+                // Run consensus
+                boolean success = runConsensus(transaction);
+                String responseMessage = success ? "Transaction confirmed and appended." : "Transaction failed.";
+                String formattedMessage = "<append:" + messageElements[1] + ":" + responseMessage + ">";
+                // System.out.println(formattedMessage);
+    
+                // Send confirmation response back to client
+                int destId = Integer.parseInt(message.split("\\|")[0]);
+                perfectLinks.send(destId, formattedMessage, seqNumber);
+                seqNumber++;
             }
         }
-
-        System.out.println("PROPOSE phase: " + transaction);
-        return true;
+        //why static
+        private static boolean runConsensus(String transaction) {
+            System.out.println("Running consensus: INIT -> PROPOSE -> DECIDE");
+    
+            if (!initConsensus())
+                return false;
+            if (!proposeConsensus(transaction))
+                return false;
+            return decideConsensus(transaction);
+        }
+        //why static
+        private static boolean initConsensus() {
+            //initialize 
+            setState(new EpochSate(systemMembership.getNumberOfNodes()));
+            System.out.println("INIT phase successful.");
+            return true;
+        }
+        //why static?
+        private static boolean proposeConsensus(String transaction) {
+            //Only the leader
+    
+            if(isLeader())
+            {
+                if(getState().getVal() == null) // val == null
+                {
+                    getState().setVal(transaction); // val:=v;
+                    for(int nodeId: systemMembership.getMembershipList().keySet()) //for all q∈Π do 
+                    {
+                        // For each process q , it triggers a Send event to send a [READ] msg doing AuthPerfectLinks
+                        //TODO
+                        // Processes reply with STATE message containing its local state <valts, val, writeset>:
+                        // Leader sends READ to all processes
+                        //how to create message???
+                        //String message = "<READ|" + getEpochId() + "|" + Id + ">";
+                        //perfectLinks.send(nodeId, message, seq_number);
+                        //seq_number ++;
+                    }
+            
+                }
+            }
+    
+            System.out.println("PROPOSE phase: " + transaction);
+            return true;
+        }
+    
+        private static boolean decideConsensus(String transaction) {
+            System.out.println("DECIDE phase: Committing transaction.");
+            blockchain.add(transaction);
+            return true;
+        }
+        public static boolean isLeader() {
+            return isLeader;
+        }
+        public static EpochSate getState() {
+            return state;
     }
-
-    private static boolean decideConsensus(String transaction) {
-        System.out.println("DECIDE phase: Committing transaction.");
-        blockchain.add(transaction);
-        return true;
-    }
-    public static boolean isLeader() {
-        return isLeader;
-    }
-    public EpochSate getState() {
-        return state;
-    }
-    public void setState(EpochSate state) {
-        this.state = state;
+    public static void setState(EpochSate state) {
+        BlockchainMember.state = state;
     }
 
 }
